6bc2bfb3be72cfa9d400f62b729329607e571b94
Michael James <mikeyjeh@gmail.com>
2016-01-12 13:28:36 -0500

    add exercise3

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,11 @@
-puts "what is your name?"
+puts "What is your name?"
+
+name = gets.chomp
+
+puts "Hi #{name}!"
+
+puts "How old are you?"
+
+age = gets.chomp.to_i
+
+puts "You are #{age} and you were born in #{2015 - age}!"

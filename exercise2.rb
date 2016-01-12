2c9d2e12dd983b4cb4c639d59be5fa2b1f1998dc
Michael James <mikeyjeh@gmail.com>
2016-01-12 13:19:27 -0500

    add exercise2

---
+++ exercise2.rb
@@ -0,0 +1,7 @@
+puts "your meal was $55, your tip should be $#{55.0*0.18}!"
+
+puts "I am " + 20.to_s + " years old"
+
+puts "The result of 45628 times 7839 is #{45628*7839}"
+
+puts ((10 < 20 && 30 < 20) || !(10 == 11))
